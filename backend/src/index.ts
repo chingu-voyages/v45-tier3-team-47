8a497900f5b0be040333feb02d91367fd0e348c9
camziny <cameronziny@gmail.com>
2023-08-23 10:37:24 -0400

    create models and associations, type interfaces, and db file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,12 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, Request, Response } from "express";
 
 const app: Express = express();
 const port = 3000;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send("Running Express + TS Server !!!");
+app.get("/", (req: Request, res: Response) => {
+  res.send("Running Express + TS Server !!!");
 });
 
 app.listen(port, () => {
-    console.log(`server running : http://localhost:${port}`);
+  console.log(`server running : http://localhost:${port}`);
 });
---
+++ backend/src/types/index.d.ts
@@ -0,0 +1,27 @@
+export interface IUser {
+  user_name?: string;
+  occupation?: string;
+  password?: string;
+  email?: string;
+  location?: string;
+  profile_image?: string;
+}
+
+export interface IPost {
+  rating?: number;
+  comments?: string;
+}
+
+export interface IPointOfInterest {
+  title: string;
+  description?: string;
+  longitude?: number;
+  latitude?: number;
+  price?: number;
+  city?: string;
+  website?: string;
+  postal_code?: string;
+  province?: string;
+  country?: string;
+  phone_number?: string;
+}
