460ddeb50387e624000f81062d52a5cce6af9415
DanielaParra <dparralp396@gmail.com>
2023-08-26 19:57:16 -0400

    Set up Router, root layout and about page Route

---
+++ frontend/src/Layout/Components/Footer.tsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+    return (
+        <footer className="flex justify-center">
+            <p>Made with 💖 by Cam, Chris, Daniela, Mays and Mehmet</p>
+        </footer>
+    )
+}
+
+export default Footer
