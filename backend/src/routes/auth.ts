49c34ac2fa1af07e0e4f237e88015ed61503f917
mays4 <maysalani84@yahoo.com>
2023-09-02 22:30:42 -0400

    Deleted unused file

--- backend/src/routes/auth.ts
+++
@@ -1,6 +0,0 @@
-import express  from "express";
-const router = express.Router();
-import { loginUser } from "../config/controllers/user.controller";
-router.post('/login',loginUser)
-
-export  default router;
