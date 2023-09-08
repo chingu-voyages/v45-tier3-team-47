017776fbec072f17fd5dccfabf10984235c65702
MeTe <25986959+mtoprak53@users.noreply.github.com>
2023-09-08 17:02:41 -0400

    fixes in db.ts

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -5,15 +5,29 @@ import dotenv from "dotenv";
 dotenv.config({ path: './.env' });
 
 const database: string = process.env.DB_NAME || "sightseeshare_db";
-const username: string = process.env.DB_USER || "default_username";
-const password: string = process.env.DB_PASS || "default_password";
+const username: string = process.env.DB_USER;
 const host: string = process.env.DB_HOST || "127.0.0.1";
 const port: string = process.env.DB_PORT || "5432";
 const dialect: string = process.env.DB_DIALECT || "postgres";
 
-const sequelize: Sequelize = new Sequelize(
-  `${dialect}://${username}:${password}@${host}:${port}/${database}`
-);
+let sequelize: Sequelize;
+
+if (process.env.DB_PASS) {
+  // If DB_PASS is truthy (not an empty string or undefined)
+  const password: string = process.env.DB_PASS;
+
+  sequelize = new Sequelize(
+    `${dialect}://${username}:${password}@${host}:${port}/${database}`
+  );
+} else {
+  // If DB_PASS is falsy (empty string or undefined)
+  sequelize = new Sequelize({
+    database: "sightseeshare_db",
+    host: "localhost",
+    port: 5432,
+    dialect: "postgres",
+  });
+}
 
 const testDbConnection = async (): Promise<void> => {
   try {
