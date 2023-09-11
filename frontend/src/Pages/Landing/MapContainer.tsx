f2e9a8fe9873de3045a7c60d5bad59dceb904ae7
camziny <cameronziny@gmail.com>
2023-09-11 16:35:20 +0100

    Add axios for requests - create axiosConfig file - change handleLogin to user axios instead of fetch for Post request - use sessionStorage within axios instance for extract userId - temporarily remove ensureCorrectUser from getUserData route

--- frontend/src/Pages/Landing/MapContainer.tsx
+++ frontend/src/Pages/Landing/MapContainer.tsx
@@ -1,47 +1,43 @@
-import { useRef, useEffect, useState } from 'react';
-import mapboxgl from 'mapbox-gl';
+import { useRef, useEffect, useState } from "react";
+import mapboxgl from "mapbox-gl";
 mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN;
 type MarkerWithId = mapboxgl.Marker & { id: string };
 type PopupWithId = mapboxgl.Popup & { id: string };
 
 type Props = {
-    longitude: number,
-    latitude: number
-}
+  longitude: number;
+  latitude: number;
+};
 
 const MapComponent = ({ longitude, latitude }: Props) => {
+  const mapContainer = useRef<any>(null);
+  const map = useRef<mapboxgl.Map | null>(null);
+  const [zoom, _] = useState(5);
 
-    const mapContainer = useRef<any>(null);
-    const map = useRef<mapboxgl.Map | null>(null)
-    const [zoom, _] = useState(5);
+  useEffect(() => {
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/streets-v12",
+      center: [longitude, latitude],
+      zoom: zoom,
+    });
 
-    useEffect(() => {
-        map.current = new mapboxgl.Map({
-            container: mapContainer.current,
-            style: 'mapbox://styles/mapbox/streets-v12',
-            center: [longitude, latitude],
-            zoom: zoom
-        })
+    // marker1 and popup1 are currently not rendered on map. Below is template to use
+    const marker1 = new mapboxgl.Marker() as MarkerWithId;
+    marker1.setLngLat([-74.0445, 40.6892]);
+    // marker1.addTo(map.current);
 
+    const popup1 = new mapboxgl.Popup({ closeOnClick: false }) as PopupWithId;
+    popup1.setLngLat([-79.0747, 43.0799]);
+    popup1.setHTML("<h4>Niagara Falls</h4>");
+    // popup1.addTo(map.current);
+  }, [longitude, latitude]);
 
-        // marker1 and popup1 are currently not rendered on map. Below is template to use 
-        const marker1 = new mapboxgl.Marker() as MarkerWithId;
-        marker1.setLngLat([-74.0445, 40.6892]);
-        // marker1.addTo(map.current);
-
-        const popup1 = new mapboxgl.Popup({ closeOnClick: false }) as PopupWithId;
-        popup1.setLngLat([-79.0747, 43.0799]);
-        popup1.setHTML('<h4>Niagara Falls</h4>')
-        // popup1.addTo(map.current);
-
-    }, [longitude, latitude])
-
-
-    return (
-        <div style={{ width: "65%" }}>
-            <div ref={mapContainer} style={{ height: "600px" }}></div>
-        </div>
-    )
-}
+  return (
+    <div style={{ width: "65%" }}>
+      <div ref={mapContainer} style={{ height: "600px" }}></div>
+    </div>
+  );
+};
 
 export default MapComponent;
