b9bc06b1e085f0ad2b93a8032a897a80cd79a2e9
DanielaParra <dparralp396@gmail.com>
2023-09-16 17:40:06 -0400

    Cleaned up empty lines

--- frontend/src/Layout/RootLayout.tsx
+++ frontend/src/Layout/RootLayout.tsx
@@ -1,13 +1,9 @@
 import { Outlet } from 'react-router-dom';
-import Nav from './Components/Nav';
+import Nav from './Components/Nav/Nav';
 import Footer from './Components/Footer';
 import Box from '@mui/material/Box';
 
-
-const RootLayout = ( ) => {
-
-
-  
+const RootLayout = () => {
 
     return (
         <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', minHeight: '100vh', minWidth: '320px' }}>
